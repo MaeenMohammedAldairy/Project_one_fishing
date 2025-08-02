7e7283bf276281b2320f84246aa1a35ab1fbea4d
MaeenMohammedAldairy <maeen.mohammedaldeiry@gmail.com>
2025-08-02 17:29:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // استخدم middleware
 app.use(express.json()); // لتحليل بيانات JSON
